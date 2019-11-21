a0ed29deb01d5e35c8c80c1e92583a942828e2d6
JuseobJang <wntjq68@gmail.com>
2019-11-21 20:56:24 +0900

    sign_up condition

--- login/signup_action.php
+++ login/signup_action.php
@@ -29,6 +29,16 @@
           exit;
         };
 
+        if(strlen($student_id) != 10){?>
+          <script>
+              alert('학번은 10자리 입니다.')
+              history.back();
+          </script>
+
+        <?php
+        exit; 
+        }
+
         $query1 = "insert into member (id, pw, student_id, phone, date, permit) values ('$id', '$pw','$student_id','$phone', '$date', 0)";
 
         $result1 = $connect->query($query1);
