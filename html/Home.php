24f4d66241ba0760163f5e563134ba20549adaa9
JuseobJang <wntjq68@gmail.com>
2019-11-21 19:18:34 +0900

    modify login

--- html/Home.php
+++ html/Home.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php session_start(); ?>
 <html lang="en">
 
 <head>
@@ -15,6 +16,7 @@
       window.open(pop_url,"",pop_option);
     };
   </script>
+
   <link href="https://fonts.googleapis.com/css?family=Abel|Farro|Lato|Roboto&display=swap" rel="stylesheet">
 
   <meta charset="utf-8">
@@ -22,6 +24,10 @@
 </head>
 
 <body>
+  <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+  ?>
 
   <nav>
     <a href="Home.html"><img src="../images/selab.png" align="center" width="72" height="26"></a>
@@ -72,49 +78,50 @@
       </p></em>
     </section>
   </article>
-
-
   <?php if (!isset($_SESSION['id'])) { ?>
-    <div id='login'>
-      <h2>SIGNUP</h2>
-      <form  action="../login/loginaction.php" method="POST">
-          <input name="id" type='text' placeholder='Username'>
-          <input name="pw" type='password' placeholder='Password'>
+      <div id='login'>
+        <h2>SIGNUP</h2>
+        <form  action="../login/loginaction.php" method="POST">
+            <input name="id" type='text' placeholder='Username'>
+            <input name="pw" type='password' placeholder='Password'>
+          <div class='divider'></div>
+          <div class=button><button type="submit">LOG IN</button></div>
+          <p>Don't have an account? <a href="javascript:popOpen();"><span class='reg'>Register.</span></a> </p>
+        </form>
+      </div>
+    <?php } else {?>
+      <div id = 'member'>
+        <?php
+        $id = $_SESSION['id'];
+        $phone = $_SESSION['phone'];
+        $student_id = $_SESSION['student_id'];
+        echo "<p>HELLO! <strong>$id</strong> <a href=\"../login/logout.php\">로그아웃</a></p> ";
+      ?>
         <div class='divider'></div>
-        <div class=button><button type="submit">LOG IN</button></div>
-        <p>Don't have an account? <a href="javascript:popOpen();"><span class='reg'>Register.</span></a> </p>
-      </form>
-    </div>
-  <?php } else {?>
-    <div id = 'member'>
-      <?php
-      $id = $_SESSION['id'];
-      $phone = $_SESSION['phone'];
-      $student_id = $_SESSION['student_id'];
-      echo "<p>HELLO! <strong>$id</strong> <a href=\"../login/logout.php\">로그아웃</a></p> ";
-    ?>
-      <div class='divider'></div>
 
-      <div id="infos">
-        <div class="info">
-          <p>Name :</p>
-        </div>
-        <div class="info">
-          <p>phone : <?php echo "$phone" ?></p>
-        </div>
-        <div class="info">
-          <p>Attendance :</p>
-        </div>
-        <div class="info">
-          <p>Student_id : <?php echo "$student_id" ?></p>
+        <div id="infos">
+          <div class="info">
+            <p>Name :</p>
+          </div>
+          <div class="info">
+            <p>phone : <?php echo "$phone" ?></p>
+          </div>
+          <div class="info">
+            <p>Attendance :</p>
+          </div>
+          <div class="info">
+            <p>Student_id : <?php echo "$student_id" ?></p>
+          </div>
         </div>
       </div>
-    </div>
-  <?php
-  } ?>
-  <footer>
-    <p>COPYRIGHT 2014 SELAB, ALL RIGHTS RESERVED. COMPUTER SCIENECE AND ENGINEERING, HANYANG UNIV. LOCATION: ENGINEERING BUILDING #3, ROOM 421. T +82-31-400-4754</p>
-  </footer>
+    <?php
+    } ?>
+
+
+
+
+
+
 
 </body>
 
