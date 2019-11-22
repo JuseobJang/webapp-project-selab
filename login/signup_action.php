01dd30d978f4b096b35e254849ddddd54081bf5c
JuseobJang <wntjq68@gmail.com>
2019-11-22 13:13:52 +0900

    format php

--- login/signup_action.php
+++ login/signup_action.php
@@ -1,6 +1,6 @@
 <?php
 
-        $connect = mysqli_connect("localhost","root","root","database","8889") or die("fail");
+        $connect = mysqli_connect("localhost", "root", "root", "database", "8889") or die("fail");
 
 
         $id=$_POST[id];
@@ -12,16 +12,16 @@
 
         $query = "select * from member where id='$id'";
         $result = $connect->query($query);
-        if(mysqli_num_rows($result)==1){
-          ?> <script>
+        if (mysqli_num_rows($result)==1) {
+            ?> <script>
               alert('이미 존재하는 유저명 입니다.')
               history.back();
             </script>
           <?php
           exit;
         };
-        if($pw != $pw_cf){
-        ?> <script>
+        if ($pw != $pw_cf) {
+            ?> <script>
             alert('비밀번호가 일치 하지 않습니다.')
             history.back();
           </script>
@@ -29,33 +29,34 @@
           exit;
         };
 
-        if(strlen($student_id) != 10){?>
+        if (strlen($student_id) != 10) {?>
           <script>
               alert('학번은 10자리 입니다.')
               history.back();
           </script>
 
         <?php
-        exit; 
+        exit;
         }
 
         $query1 = "insert into member (id, pw, student_id, phone, date, permit) values ('$id', '$pw','$student_id','$phone', '$date', 0)";
 
         $result1 = $connect->query($query1);
 
-        if($result1) {
-        ?>      <script>
+        if ($result1) {
+            ?>      <script>
                 alert('가입 되었습니다.');
                 window.close();
                 </script>
 
-<?php   }
-        else{
-?>              <script>
+<?php
+        } else {
+            ?>              <script>
                         alert("양식을 다 작성해주세요.");
                         history.back();
                 </script>
-<?php   }
+<?php
+        }
 
         mysqli_close($connect);
 ?>
