e16fce5a0d139ccee6fa1142a47e0147261a7db5
은지 <qaqa3624@naver.com>
2019-12-01 23:14:12 +0900

    admin

--- php/input_grade.php
+++ php/input_grade.php
@@ -4,9 +4,46 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/input_grade.css">
+        <link rel="stylesheet" href="../css/menu.css">
+        <link rel="stylesheet" href="../css/layout.css">
         <title>Software Engineering Lab - Courses</title>
     </head>
     <body>
+
+    <nav>
+    <a href="Home.php"><img src="../images/selab.png" align="center" width="72" height="26"></a>
+    <ul>
+      <li><a href="Contact.php">Contact</a></li>
+      <li>
+        <?php if (isset($_SESSION['id'])){ ?> 
+          <a href="../qa/board/index.php">Q & A</a>
+        <?php }
+         else{ ?> 
+          <a onClick = "alert('로그인 해주세요.')" >Q & A</a> 
+          <?php }?>
+      </li>
+      <li><a href="Courses.php">Courses</a>
+      </li>
+      <li><a href="Publications.php">Publications</a>
+        <ul>
+          <li><a href="Pub_incon.php">International Conference</a></li>
+          <li><a href="Pub_injour.php">International Journal</a></li>
+          <li><a href="Pub_domcon.php">Domestic Conference</a></li>
+          <li><a href="Pub_domjour.php">Domestic Journal</a></li>
+        </ul>
+      </li>
+      <li><a href="Research.php">Research</a></li>
+      <li><a href="Members.php">Members</a></li>
+      <li><a href="Notice.php">Notice</a></li>
+    </ul>
+  </nav>
+
+  <header>
+    <h1>ADMIN PAGE</h1>
+  </header>
+
+  <h2>MEMBER'S GRADE</h2>
 
     <?php
      $primary = $_POST['input_grade'];
--- php/webapp_admin.php
+++ php/webapp_admin.php
@@ -7,11 +7,46 @@
     <title>Web App - Admin page</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="../css/webapp_admin.css">
+    <link rel="stylesheet" href="../css/menu.css">
+    <link rel="stylesheet" href="../css/layout.css">
 </head>
 
 <body>
+<nav>
+    <a href="Home.php"><img src="../images/selab.png" align="center" width="72" height="26"></a>
+    <ul>
+      <li><a href="Contact.php">Contact</a></li>
+      <li>
+        <?php if (isset($_SESSION['id'])){ ?> 
+          <a href="../qa/board/index.php">Q & A</a>
+        <?php }
+         else{ ?> 
+          <a onClick = "alert('로그인 해주세요.')" >Q & A</a> 
+          <?php }?>
+      </li>
+      <li><a href="Courses.php">Courses</a>
+      </li>
+      <li><a href="Publications.php">Publications</a>
+        <ul>
+          <li><a href="Pub_incon.php">International Conference</a></li>
+          <li><a href="Pub_injour.php">International Journal</a></li>
+          <li><a href="Pub_domcon.php">Domestic Conference</a></li>
+          <li><a href="Pub_domjour.php">Domestic Journal</a></li>
+        </ul>
+      </li>
+      <li><a href="Research.php">Research</a></li>
+      <li><a href="Members.php">Members</a></li>
+      <li><a href="Notice.php">Notice</a></li>
+    </ul>
+  </nav>
+
+  <header>
+    <h1>ADMIN PAGE</h1>
+  </header>
+
+    <h2>MEMBER'S LIST</h2>
+
     <div id="member_list">
-        <p>hi</p>
         <?php
         $connect = mysqli_connect("localhost", "root", "root", "database", "8889") or die("fail");
         $query = "select * from member where course='Web Application Development';";
@@ -19,7 +54,7 @@
 
 
         echo '<table class="text-center"><tr>' .
-            '<th>성</th><th>이름</th><th>학번</th><th>반</th><th></th>' .
+            '<th>Last_name</th><th>First_name</th><th>Student_id</th><th>Class</th><th></th>' .
             '</tr>';
         while ($row = mysqli_fetch_array($result)) {
             $primary = $row['student_id'];
