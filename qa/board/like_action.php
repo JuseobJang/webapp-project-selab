b4f0a27c79b87da56b9cbb20e8146a38c92c1c34
jekyun-park <parkjekyun@gmail.com>
2019-12-05 21:35:37 +0900

    bug

--- qa/board/like_action.php
+++ qa/board/like_action.php
@@ -20,26 +20,7 @@ $likes = $row['likes'] ;
 $unlikes = $row['unlikes'];
 
 
-// if(!empty($bNo) && empty($_COOKIE['board_free_like' . $likes]) ){ //글번호가있고, likes라는 쿠키가없으면
-//     if($value == 1){
-//         $likes = $likes +1;
-//         $query1 = "update board_free set likes = $likes where b_no='$b_no'";
-//         $result1 = $connect->query($query1);
-//     }
-//     else{
-//         exit;
-//     } 
-// }
-// else if(!empty($bNo) && empty($_COOKIE['board_free_unlike' . $unlikes]) ){
-//     if($value == 0){
-//         $unlikes = $unlikes +1;
-//         $query1 = "update board_free set unlikes = '$unlikes' where b_no='$b_no'";
-//         $result1 = $connect->query($query1);
-//     }
-//     else{
-//         setcookie('board_free_unlike' . $unlikes, TRUE, time() + (60 * 60 * 24), '/');
-//     }
-// }
+
 
 if($value == 1){
     $likes = $likes +1;
